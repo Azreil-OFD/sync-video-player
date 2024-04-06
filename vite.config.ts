e38e52ec72eb0eb4f0eea990a87121740974adbe
Azreil-OFD <fantom2413@gmail.com>
2024-04-07 01:03:11 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,22 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  esbuild: {
+    logLevel: 'debug'
+  },
   plugins: [vue()],
   build: {
+    
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'MultiVideoPlayer',
       fileName: (format) => `sync-video-player.${format}.js`,
       formats: ['es', 'umd']
     },
-    rollupOptions: {
+    rollupOptions: { 
+      input: {
+        main: './index.html',
+      },
       external: ['vue'],
       output: {
         globals: {
